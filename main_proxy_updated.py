ef759d3ff11bd6917de82bd8a91b62a4566230e7
mohan9638 <158275364+mohan9638@users.noreply.github.com>
2025-09-24 17:22:10 +0530

    Add files via upload

---
+++ main_proxy_updated.py
@@ -0,0 +1,94 @@
+# 🔧 Standard Library
+import os
+import re
+import sys
+import time
+import json
+import random
+import string
+import shutil
+import zipfile
+import urllib
+import subprocess
+from datetime import datetime, timedelta
+from base64 import b64encode, b64decode
+from subprocess import getstatusoutput
+
+# 🕒 Timezone
+import pytz
+
+# 📦 Third-party Libraries
+import aiohttp
+import aiofiles
+import requests
+import asyncio
+import ffmpeg
+import m3u8
+import cloudscraper
+import yt_dlp
+import tgcrypto
+from bs4 import BeautifulSoup
+from pytube import YouTube
+from Crypto.Cipher import AES
+from Crypto.Util.Padding import unpad
+
+# ⚙️ Pyrogram
+from pyrogram import Client, filters, idle
+from pyrogram.handlers import MessageHandler
+from pyrogram.types import (
+    Message,
+    CallbackQuery,
+    InlineKeyboardMarkup,
+    InlineKeyboardButton
+)
+from pyrogram.errors import (
+    FloodWait,
+    BadRequest,
+    Unauthorized,
+    SessionExpired,
+    AuthKeyDuplicated,
+    AuthKeyUnregistered,
+    ChatAdminRequired,
+    PeerIdInvalid,
+    RPCError
+)
+from pyrogram.errors.exceptions.bad_request_400 import MessageNotModified
+
+# 🧠 Bot Modules
+import auth
+import ug as helper
+from ug import *
+
+from clean import register_clean_handler
+from logs import logging
+from utils import progress_bar
+from vars import *
+from pyromod import listen
+import apixug
+from apixug import SecureAPIClient
+from db import db
+
+# ... (unchanged code above)
+
+Vxy = links[i][1].replace("file/d/","uc?export=download&id=").replace("www.youtube-nocookie.com/embed", "youtu.be").replace("?modestbranding=1", "").replace("/view?usp=sharing","")
+original_url = "https://" + Vxy
+link0 = original_url
+url = f"https://hls-proxy-7x6f.onrender.com/stream?url={original_url}"
+
+name1 = links[i][0].replace("(", "[").replace(")", "]").replace("_", "").replace("\t", "").replace(":", "").replace("/", "").replace("+", "").replace("#", "").replace("|", "").replace("@", "").replace("*", "").replace(".", "").replace("https", "").replace("http", "").strip()
+if "," in raw_text3:
+     name = f'{PRENAME} {name1[:60]}'
+else:
+     name = f'{name1[:60]}'
+
+# ... (unchanged code)
+
+Vxy = link.replace("file/d/","uc?export=download&id=").replace("www.youtube-nocookie.com/embed", "youtu.be").replace("?modestbranding=1", "").replace("/view?usp=sharing","")
+original_url = Vxy
+link0 = original_url
+url = f"https://hls-proxy-7x6f.onrender.com/stream?url={original_url}"
+
+name1 = links.replace("(", "[").replace(")", "]").replace("_", "").replace("\t", "").replace(":", "").replace("/", "").replace("+", "").replace("#", "").replace("|", "").replace("@", "").replace("*", "").replace(".", "").replace("https", "").replace("http", "").strip()
+name = f'{name1[:60]}'
+
+# ... (rest of your existing code remains unchanged) ...
